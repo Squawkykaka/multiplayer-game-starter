b3ce6e1b1c4110fa77b925d134e0e6fc41856f4b
Squawkykaka <contact@squawkykaka.com>
2023-11-15 09:49:32 +1300

    Done most of part 3

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(join(__dirname + '/index.html'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const players = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  players[socket.id] = {
+    x: 100,
+    y: 100
+  }
+
+  io.emit('updatePlayers', players)
+
+  console.log(players)
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
 })
